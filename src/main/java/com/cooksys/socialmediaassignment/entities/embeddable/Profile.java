dfe1d6cd580ae7cb7d1b711f09e7d5247d51580a
Joshua Shin <jshin1223@gmail.com>
2023-01-30 18:09:05 -0500

    Tweet entities dto controller

---
+++ src/main/java/com/cooksys/socialmediaassignment/entities/embeddable/Profile.java
@@ -0,0 +1,20 @@
+package com.cooksys.socialmediaassignment.entities.embeddable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+}
