02803d83d288da94e889944cf1a9fe2470692882
Jason Layton <16jalayt@gmail.com>
2023-01-31 19:01:03 -0600

    Attempt to add more validate endpoints, will have to fix after merge

--- src/main/java/com/cooksys/socialmediaassignment/controllers/ValidateController.java
+++ src/main/java/com/cooksys/socialmediaassignment/controllers/ValidateController.java
@@ -1,5 +1,7 @@
 package com.cooksys.socialmediaassignment.controllers;
 
+import com.cooksys.socialmediaassignment.dtos.HashtagResponseDto;
+import com.cooksys.socialmediaassignment.services.HashtagService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +14,25 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/validate")
-public class ValidateUserController {
+public class ValidateController {
 
 	private final UserService userService;
+	private final HashtagService hashtagService;
 
 	@GetMapping("/username/exists/@{username}")
 	public boolean checkUserByUsername(@PathVariable String username) {
 		return userService.checkUserByUsername(username);
+	}
+
+	@GetMapping("/username/available/@{username}")
+	public boolean checkUsernameAvailable(@PathVariable String username) {
+		return false;
+	}
+
+	@RequestMapping(value = "/tag/exists/{label}")
+	@GetMapping
+	public HashtagResponseDto validateTagExists(@PathVariable("label") String label) {
+		return hashtagService.validateTagExists(label);
 	}
 
 }
